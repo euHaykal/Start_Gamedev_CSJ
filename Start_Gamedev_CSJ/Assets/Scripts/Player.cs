51d62c83ba17ac39df35f06dc62bf073caf7ca67
Haykal <moragasfarage.haykal@gmail.com>
2025-04-16 13:47:33 -0300

    Player Running e Rolling

--- Start_Gamedev_CSJ/Assets/Scripts/Player.cs
+++ Start_Gamedev_CSJ/Assets/Scripts/Player.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
 
     private Rigidbody2D rig;
+
+    private float initialSpeed;
+    private bool _isRunning;
+    private bool _isRolling;
     private Vector2 _direction;  //Encapsulamento
 
+    public bool isRunning  //Encapsulamento
+    {
+        get { return _isRunning; }  //Pega o valor da váriável _isRunning
+        set { _isRunning = value; }  //Seta o valor da variável isRunning para o mesmo da variável _isRunning
+    }
+    public bool isRolling  //Encapsulamento
+    {
+        get { return _isRolling; }  //Pega o valor da váriável _isRolling
+        set { _isRolling = value; }  //Seta o valor da variável isRolling para o mesmo da variável _isRolling
+    }
     public Vector2 direction  //Encapsulamento
     {
         get { return _direction; }  //Pega o valor da váriável _direction
@@ -17,16 +32,63 @@ public class Player : MonoBehaviour
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();  //No inicio da cena, o unity ira procurar pelo combonente Rigidbody2D no objeito personagem
+        initialSpeed = speed;
     }
 
     private void Update()
     {
-        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));  //Armazena informação quando o input horizontal ou vertical é acionado
+        OnInput();
+        OnRun();
+        OnRolling();
     }
 
     private void FixedUpdate() //Só utiliza para coisas com física
     {
+        OnMove();
+    }
+
+    #region Movement
+
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));  //Armazena informação quando o input horizontal ou vertical é acionado
+    }
+
+    void OnMove()
+    {
         rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime); //Acessa a posição do personagem e adiciona ao input acionado
     }
+
+    void OnRun()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))  //Se o Shift esquerdo é pressionado, a variável speed recebe o valor da variável runSpeed
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift)) //Quando o Shift esquerdo é pressionado, a variável speed volta a receber o valor da variável initialSpeed
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    void OnRolling()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            speed = runSpeed;
+            _isRolling = true;
+        }
+
+                if(Input.GetMouseButtonUp(1))
+        {
+            speed = initialSpeed;
+            _isRolling = false;
+        }
+    }
+
+    #endregion
 
 }
--- Start_Gamedev_CSJ/Assets/Scripts/PlayerAnim.cs
+++ Start_Gamedev_CSJ/Assets/Scripts/PlayerAnim.cs
@@ -13,13 +13,28 @@ public class PlayerAnim : MonoBehaviour
 
     void Update()
     {
-        if(player.direction.sqrMagnitude > 0)  //Código para verificar está clicando input de andar ou não
+       OnMove();
+       OnRun();
+    }
+
+    #region Movement
+     void OnMove()
+    {
+         if(player.direction.sqrMagnitude > 0)  //Código para verificar está clicando input de andar ou não
         {
-            anim.SetInteger("transition",1);
+            if(player.isRolling)  //Checa se o player está rolando
+            {
+                anim.SetTrigger("isRoll");  //Animator transition = 3
+            }
+            else
+            {
+                anim.SetInteger("transition",1);  //Animator transition = 1
+            }
+            
         }
         else
         {
-            anim.SetInteger("transition",0);
+            anim.SetInteger("transition",0);  //Animator transition = 0
         }
 
         if(player.direction.x > 0)  //Código para verificar se o jogador está andando para a direita. Reflete a arte do personagem para a direção correta do movimento
@@ -33,4 +48,14 @@ public class PlayerAnim : MonoBehaviour
         }
     }
 
+    void OnRun()
+    {
+        if(player.isRunning)
+        {
+            anim.SetInteger("transition", 2);  //Animator transition = 2
+        }
+    }
+
+    #endregion
+   
 }
